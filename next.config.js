52aa223ef55cb6832293e0786c2b5922b60df903
Paul Scanlon <pauliescanlon@gmail.com>
2023-09-11 11:24:41 -0400

    chore: add redirect

--- next.config.js
+++ next.config.js
@@ -3,4 +3,11 @@ const isProd = process.env.NODE_ENV === 'production';
 module.exports = {
   assetPrefix: isProd ? process.env.NEXT_PUBLIC_REWRITE_PREFIX : undefined,
   basePath: isProd ? process.env.NEXT_PUBLIC_REWRITE_PREFIX : undefined,
+  redirects: [
+    {
+      source: '/',
+      destination: process.env.NEXT_PUBLIC_REWRITE_PREFIX,
+      permanent: true,
+    },
+  ],
 };
